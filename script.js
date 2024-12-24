deaa27b6a508a6025538f7a322bb850d9b6815f5
Senjuv <senjusd49@gmail.com>
2024-12-23 21:30:09 -0600

    v.1

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', () => {
         // Animar las piezas hacia el centro para formar el corazón
         pieces.forEach((piece) => {
             piece.style.opacity = 1; // Hacer visible la pieza
-            piece.style.transform = 'translate(0, 0) rotate(360deg) scale(1)'; // Mover, rotar y escalar a su posición dentro del grid
+            piece.style.transform = 'translate(0, 0) rotate(0deg) scale(1)'; // Mover, rotar y escalar a su posición dentro del grid
         });
     });
 });
